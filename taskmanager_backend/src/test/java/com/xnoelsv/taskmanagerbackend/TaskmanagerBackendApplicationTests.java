5058bddf93b19e21c58179c1a59e6d77d2db509a
xNoelSV <132565269+xNoelSV@users.noreply.github.com>
2025-10-15 12:26:58 +0200

    Initial commit

---
+++ taskmanager_backend/src/test/java/com/xnoelsv/taskmanagerbackend/TaskmanagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xnoelsv.taskmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
